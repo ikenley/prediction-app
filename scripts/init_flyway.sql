4f9b4611bd9a7cf8bd297ef1c83d2210fee80ee6
ikenley <ikenley6@gmail.com>
2023-03-25 18:00:16 -0400

    flyway codebuild

--- scripts/init_flyway.sql
+++ scripts/init_flyway.sql
@@ -2,6 +2,8 @@
 -- Boostrapping scripts needed to get migrations running
 --
 
-create database antigone;
+-- create database antigone;
 
-CREATE USER flyway_admin WITH PASSWORD 'TODO_SET_PASSOWORD' CREATEROLE;
+CREATE USER flyway_admin WITH PASSWORD 'DrCwrTRZ7mYEBVTY8Un9E80QQFmBlXY5' CREATEROLE;
+
+grant all privileges on database core to flyway_admin;
