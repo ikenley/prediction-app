73c07279e4a4faae1db7f0c060c2060ddf14e3cb
ikenley <ikenley6@gmail.com>
2025-06-19 18:56:02 +0100

    fix front-end build (#43)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import App from "./App";
 
